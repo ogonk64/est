1ed427ac854ee5e67393af8859dc1b3e8da6d13e
ogonk64 <46124789+ogonk64@users.noreply.github.com>
2018-12-25 09:56:45 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+defined("_JEXEC") or die();
+$doc=JFactory::getDocument();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		
+		<title>est</title>
+		<link  type="text/css" rel="stylesheet" href="D:\Disk_D\Олег\My site est\KharkovEst\css\est_css.css"/>
+	</head>
+	<body>
+	<div id="header">
+  <div class="logo"><img src="D:\Disk_D\Олег\My site est\KharkovEst\images\kharkov.jpg" width="370" height="100" /></div>
+	  <div class="header_est">
+	     <h1 class="liter">Недвижимость Харькова</h1>
+	   </div>
+	   <div class="img"><img src="D:\Disk_D\Олег\My site est\KharkovEst\images\Building.jpg" width="365" height="100"/></div>
+	</div>
+	<div class="menu_gen">
+	<nav id="topnav" role="navigation">
+          
+	          <ul class="srt-menu" id="menu-main-navigation">
+              <li class="current"><a href="/">ГЛАВНАЯ</a></li><li><a href="#">КВАРТИРЫ</a><ul><li><a href="">1-комнатные</a></li><li><a href="">2-комнатные</a></li><li><a href="">3-комнатные</a></li><li><a href="">4-комнатные</a></li><li><a href="">Комнаты, гостинки</a></li></ul></li><li><a href="#">ДОМА</a><ul><li><a href="">Дома в городе</a></li><li><a href="">Дома в пригороде</a></li>></ul></li><li><a href="#">АРЕНДА</a><ul><li><a href="">КВАРТИРЫ</a></li><li><a href="">ДОМА</a></li><li><a href="/#">ГОСТИНКИ</a></li></ul></li><li><a href="">КОНТАКТЫ</a></li><li>
+                  <a href="#">ПРОЧЕЕ</a>
+              </li>
+
+          </ul>
+		</nav>
+
+</div>
+		
+	
+	<div class="content"><div class="contaner"<p class="one"><h1 class="two">        Содержание сайта</h1><br/>Уважаемые посетители сайта</p></div></div>
+		
+	<div id="footer"><span class="ftn">@copyrite 2018</span></div>
+		
+	</body>
+  
+  </html>
+</html>
